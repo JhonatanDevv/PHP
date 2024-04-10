3112c577af72c74e07ab6aeee4ec09e0c0e4cdee
Jhonatan Marco <you@example.comjholxd@hotmail.com>
2024-04-10 00:55:29 -0300

    Primeiro commit

---
+++ regra de indentificador.php
@@ -0,0 +1,17 @@
+#1 Variavel sempre começa com o simbolo de - $.
+
+
+#2 O segundo pode ser letra ou o simbolo _.
+
+
+#3 Aceita caracteres [a-z],[A-Z],[0-9],[_].
+
+
+#4 Aceita caracteres da tabela ASCII a partir de 128
+
+
+#5 Aceita caracteres acentuados como á, õ, ç
+
+#6 A linguagem é case sensitive em relação aos nomes
+
+#7 NOmes especias como $this não podem ser usados
