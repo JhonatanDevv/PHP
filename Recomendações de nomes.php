8e86cfd3efd438c0e475fe7034a8a810be9d49ef
Jhonatan Marco <you@example.comjholxd@hotmail.com>
2024-04-10 00:55:29 -0300

    Primeiro commit

---
+++ Recomendações de nomes.php
@@ -0,0 +1,13 @@
+#1 Tente dar nomes 'claros' e de 'fácil' indentificação
+
+#2 Evite nomes muitos 'curtos' ou muito 'longos'
+
+#3 Defina um 'padrão' e siga em todo o projeto
+
+#4 Para 'váriaveis', de preferência a letras 'minúsculas'
+
+#5 Para 'constantes', de preferência a letras maiúsculas
+
+#6 Use camelCase para métodos e atributos -'$nomeCompletoCLiente'
+
+#7 Use o SNAKE_CASE para nomear constantes -'$telefone_contato_fornecedor'
---
+++ tiposPrimitivosEscalares.php
@@ -0,0 +1,29 @@
+$sobrenome = "Jhonatan" - string sequencia de letras, numeros simbolos, sempre representados com aspas
+
+$idade = "26" - int ou integer - valor numerico inteiro, aquele que vem sem a parte decimal
+
+$peso = "90.4" - float, double ou real - real só existe antes da versao 7.4. - um valor numerico real, vem com a parte decimal, depois do ponto flutuante.
+
+$casado = true - bool ou boolean, - um valor lógico ou booleano, que aceita apenas os valores verdadeiro ou falso (true or false).
+
+
+
+'rj' string
+
+3.1415 float ou double
+
+17 int
+
+true bool
+
+"" string
+
+-19 integer ou int
+
+"false" string
+
+0x1A int
+
+3e2 double
+
+"1024" string
