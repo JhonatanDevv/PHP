86837b892379701f8105a8015c32d1192d3c368c
JhonatanDevv <jholxd@hotmail.com>
2024-05-20 22:41:22 -0300

    Curso em video

---
+++ operacoesAritimetica.php
@@ -0,0 +1,27 @@
+ 
+ <!-- <?php
+ //$r = abs(-200-);
+ //print("a resposta é $r"); -->
+
+ //$r = base_convert(254, 10, 8);
+ //print("a resposta é $r");
+
+ //$r = base_convert(254, 10, 16);
+ //print("a resposta é $r");
+
+ //$r = base_convert(254, 10, 82);
+ //print("a resposta é $r");
+
+
+//  intdiv
+// $r = 5%2;
+// print("a resposta é $r");
+
+// $r = intdiv(5,2);
+// print("a resposta é $r");
+// <!-- min and max values -->
+$r = min (8,9,3,5,1,9,8,4,3);
+print("a resposta é $r");
+
+
+
---
+++ operadorAritimetico.php
@@ -0,0 +1,21 @@
+<?php
+//  $r = "2"+"2";
+//  var_dump($r);
+//  echo $r;
+
+// Quando quiser concatenar usar o '' . ''
+// $r ="10" + "101";
+// var_dump($r);
+
+
+
+// + adicao  5+2 = 7
+// - subtracao 5-2 = 3
+// * multilicação 5 * 2 = 10
+// / divisão real 5 / 2 = 2.5
+// % Módulo 5 / 2 = 1
+// ** potência 5 ** 2 = 25 -- não funciona antes da versão 5.6
+
+$r = 50/2+3**2;
+
+echo $r;
